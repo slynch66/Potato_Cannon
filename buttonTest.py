8c6c8a0b2ff77bd1cce8164f1cf1c8fb0ec0c87c
slynch66 <55889829+slynch66@users.noreply.github.com>
2022-03-03 11:49:45 -0500

    Update buttonTest.py

--- buttonTest.py
+++ buttonTest.py
@@ -3,5 +3,7 @@ from gpiozero import Button
 # Declaring the button pin on pin 17
 button = Button(11)
 
-if button.is_pressed:
-  print("The button code works!")
+while True:
+  
+  if button.is_pressed:
+    print("The button code works!")
