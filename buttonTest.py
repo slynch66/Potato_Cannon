e972b636305921e5511c3d5e8d1d1bb7a2c5e7d0
slynch66 <slynch66@charlottesvilleschools.org>
2022-03-03 17:27:01 +0000

    booty

--- buttonTest.py
+++ buttonTest.py
@@ -1,9 +1,17 @@
 from gpiozero import Button
+from signal import pause
 
 # Declaring the button pin on pin 17
-button = Button(11)
+button = Button(17)
+
+
+#def does_it_work():
+#	print("It works!")
+
+#button.when_pressed = does_it_work()
 
 while True:
   
   if button.is_pressed:
     print("The button code works!")
+pause()
