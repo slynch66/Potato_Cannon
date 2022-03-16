f03a337b8ef2a31cf43f20ae4b23d18d54d39528
slynch66 <55889829+slynch66@users.noreply.github.com>
2022-03-16 11:32:58 -0400

    Update servo_test.py

--- servo_test.py
+++ servo_test.py
@@ -1,23 +1,30 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
-GPIO.setmode(GPIO.BOARD)
+P_SERVO = 22 # adapt to your wiring
+fPWM = 50  # Hz (not higher with software PWM)
+a = 10
+b = 2
 
-# these two lines set the servo pin as pin 3 
-GPIO.setup(23, GPIO.OUT) 
-pwm=GPIO.PWM(23, 50)
+def setup():
+    global pwm
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(P_SERVO, GPIO.OUT)
+    pwm = GPIO.PWM(P_SERVO, fPWM)
+    pwm.start(0)
 
-# this makes sure it doesn't set any angles on startup
-pwm.start(0)
-
-SetAngle(90)
-SetAngle(0)
-
-def SetAngle(angle):
-	duty = angle / 18 + 2
-	GPIO.output(03, True)
-	pwm.ChangeDutyCycle(duty)
-	sleep(1)
-	GPIO.output(03, False)
-	pwm.ChangeDutyCycle(0) # this changes the duty back
-  # to 0 so that the pi isn't constantly sending signals to the servo.
+def setDirection(direction):
+    duty = a / 180 * direction + b
+    pwm.ChangeDutyCycle(duty)
+    print "direction =", direction, "-> duty =", duty
+    time.sleep(1) # allow to settle
+   
+print "starting"
+setup()
+for direction in range(0, 181, 10):
+    setDirection(direction)
+direction = 0    
+setDirection(0)    
+setDirection(90)
+GPIO.cleanup() 
+print "done"
